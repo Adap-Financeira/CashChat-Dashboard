36a317ecd7556b4d9f9c696a398e92a4f604bda3
GuilhermeGarcia <guilherme.garcia1136@gmail.com>
2025-05-18 12:22:53 -0300

    Add toaster and zod schemas

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Toaster />
           <div className="flex flex-col min-h-screen w-full max-w-[1600px] mx-auto">{children}</div>
         </ThemeProvider>
       </body>
