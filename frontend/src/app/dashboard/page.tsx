af5fcab8f20c09925528bf9ef33e73b4963d971c
GuilhermeGarcia <guilherme.garcia1136@gmail.com>
2025-05-23 16:46:01 -0300

    Add date filter component

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,24 +1,30 @@
-"use client";
-import { Button } from "@/components/ui/button";
+import DateFilter from "@/components/date-filter/DateFilter";
+import { parseDateStringsToObjects, validateOrDefaultDateStrings } from "@/utils/date";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
-  async function getUserId() {
-    const response = await fetch("http://localhost:5001/api/auth/example", {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    return data;
+export default async function Dashboard({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const { from, to } = await searchParams;
+
+  // if user only specify the from date, redirect to the same url with from = to
+  if (from && !to) {
+    redirect(`/dashboard?from=${from}&to=${from}`);
   }
 
+  // startDate and endDate will always be in the format dd/MM/yyyy
+  const { startDate, endDate } = validateOrDefaultDateStrings(from || "", to || "");
+
+  // We should call the fetch api here with the startDate and endDate
+
+  // this will return Date objects of the string dates above
+  const { startDate: startObj, endDate: endObj } = parseDateStringsToObjects(startDate, endDate);
+
   return (
-    <div>
-      <h1>Dashboardaa</h1>
-      <Button onClick={getUserId}>Get user id</Button>
+    <div className="flex flex-col gap-5 max-h-screen">
+      <DateFilter from={startObj} to={endObj} />
     </div>
   );
 }
---
+++ frontend/src/components/date-filter/DateFilter.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { DatePickerWithRange } from "../date-picker-range/DatePickerRange";
+import { Button } from "../ui/button";
+import { getToday, getCurrentWeek, getCurrentMonth, isToday, isWeek, isMonth } from "@/utils/date";
+
+interface DateFilterProps {
+  from: Date;
+  to: Date;
+}
+
+export default function DateFilter({ from, to }: DateFilterProps) {
+  const startDate = from;
+  const endDate = to;
+
+  return (
+    <div className="flex items-center justify-end gap-2 flex-wrap">
+      <Button
+        variant={isToday(startDate, endDate) ? "default" : "outline"}
+        onClick={() => {
+          redirect("/dashboard?from=" + getToday() + "&to=" + getToday());
+        }}
+      >
+        Hoje
+      </Button>
+      <Button
+        variant={isWeek(startDate, endDate) ? "default" : "outline"}
+        onClick={() => {
+          redirect("/dashboard?from=" + getCurrentWeek().startDate + "&to=" + getCurrentWeek().endDate);
+        }}
+      >
+        Semana
+      </Button>
+      <Button
+        variant={isMonth(startDate, endDate) ? "default" : "outline"}
+        onClick={() => {
+          redirect("/dashboard?from=" + getCurrentMonth().startDate + "&to=" + getCurrentMonth().endDate);
+        }}
+      >
+        Mês
+      </Button>
+      <DatePickerWithRange from={startDate} to={endDate} />
+    </div>
+  );
+}
